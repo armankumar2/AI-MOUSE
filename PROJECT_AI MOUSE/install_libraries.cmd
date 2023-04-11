7add070beb7de0dec3c5ce4744cc157c824e1ec0
armankumar2 <89579010+armankumar2@users.noreply.github.com>
2023-04-11 22:32:06 +0530

    Update install_libraries.cmd

--- PROJECT_AI MOUSE/install_libraries.cmd
+++ PROJECT_AI MOUSE/install_libraries.cmd
@@ -1,13 +1,11 @@
 :: tested on python version 3.10.7 
 :: tested on mediapipe 0.9.2.1
 :: time is inbuilt function, no need to install, just import
-:: Python Version is 3.8.0
 
 pip install opencv-python
 pip install mediapipe
 pip install numpy
 pip install pyautogui
-pip install autopy
 pip install mouse
 
 
